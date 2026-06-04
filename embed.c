7f8fc2a4c082221fad133e0f8390111df54853cf
ZippyType <endermanthecookie@gmail.com>
2026-06-04 09:36:16 +0200

    Cleaned up index and applied .gitignore

---
+++ embed.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+const uint8_t embedded_kernel[] = {0};
+const uint32_t embedded_kernel_len = 0;
+const uint8_t grub_boot_img[] = {0};
+const uint32_t grub_boot_img_len = 0;
+const uint8_t grub_core_img[] = {0};
+const uint32_t grub_core_img_len = 0;
+const uint8_t embedded_trampoline[] = {0};
+const uint32_t embedded_trampoline_len = 0;
